cbc0113f20f7a90f261bead3a51f17282addda3f
softarrow <softarrow@otiandi.com>
2019-05-03 18:22:35 +0800

    修复格式问题

--- ifSonarqube.sh
+++ ifSonarqube.sh
@@ -8,15 +8,13 @@ docker ps | grep sonarqube  postgres &> /dev/null
 #如果没有启动，使用docker-compose启动相关的sonarqube容器
 if [ $? -ne 0 ]
 then
-	echo "sonarqube is not up,we will start up it!!!"
-   wget https://github.com/jamesz2011/sonarqube/raw/master/postgres_sonarqube.yml
+  echo "sonarqube is not up,we will start up it!!!"
+  #wget https://github.com/jamesz2011/sonarqube/raw/master/postgres_sonarqube.yml
 
   #docker-compose -f postgres_sonarqube.yml up -d
-  docker-compose -f postgres_sonarqube.yml up 
-    
-
+  docker-compose -f postgres_sonarqube.yml up -d
 else
-	echo "Sonarqube is up!!!"
+  echo "Sonarqube is up!!!"
 fi
 
 echo "---------------------------------------------------------"
